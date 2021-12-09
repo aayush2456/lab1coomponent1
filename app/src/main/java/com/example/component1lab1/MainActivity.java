bfb1c1276a7848fad6c5a999e83c3a6cddbfbf23
aayush2456 <jaayush21@tbc.edu.np>
2021-12-09 20:11:07 +0545

    Implemented View

--- app/src/main/java/com/example/component1lab1/MainActivity.java
+++ app/src/main/java/com/example/component1lab1/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button countButton;
     private Button toastButton;
     private TextView textView;
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         countButton= findViewById(R.id.button_count);
         toastButton= findViewById(R.id.button_toast);
         textView= findViewById(R.id.count_text);
+        toastButton.setOnClickListener(this);
+        countButton.setOnClickListener(this);
 
 //        toastButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -30,20 +32,30 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 //
-        countButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                count++;
-                Log.d("MainActivity",""+count);
-                textView.setText(""+count);
-            }
-        });
-
-
+//        countButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                count++;
+//                Log.d("MainActivity",""+count);
+//                textView.setText(""+count);
+//            }
+//        });
+//
+//
     }
+//    public void toastClick(View view) {
+////        Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+////    }
 
-    public void toastClick(View view) {
-        Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_toast){
+            Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+        }else if(v.getId() == R.id.button_count){
+            count++;
+                Log.d("MainActivity",""+count);
+               textView.setText(""+count);
+        }
     }
 }
 
