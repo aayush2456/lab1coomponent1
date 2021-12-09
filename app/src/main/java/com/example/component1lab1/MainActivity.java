ceaab22f1f9796edbbe47d0153033a947ed911eb
aayush2456 <jaayush21@tbc.edu.np>
2021-12-09 14:30:43 +0545

    Count completed

--- app/src/main/java/com/example/component1lab1/MainActivity.java
+++ app/src/main/java/com/example/component1lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.component1lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button countButton;
     private Button toastButton;
     private TextView textView;
-
+    private int count;
 
 
     @Override
@@ -20,21 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         countButton= findViewById(R.id.button_count);
         toastButton= findViewById(R.id.button_toast);
-        textView= findViewById(R.id.count_texts);
+        textView= findViewById(R.id.count_text);
 
-        toastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
-            }
-        });
-
+//        toastButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
         countButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                count++;
+                Log.d("MainActivity",""+count);
+                textView.setText(""+count);
             }
         });
 
+
+    }
+
+    public void toastClick(View view) {
+        Toast.makeText(MainActivity.this,"Clicked",Toast.LENGTH_LONG).show();
     }
 }
+
